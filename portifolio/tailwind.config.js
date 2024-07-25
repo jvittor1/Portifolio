daca8602a8049eb8a47d1806baa72523896a0632
João Vitor <joaovitorb3105@gmail.com>
2024-07-24 23:49:17 -0300

    feat: adding packages and styles config

--- portifolio/tailwind.config.js
+++ portifolio/tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "custom-shadow": "rgba(0, 0, 0, 0.1) 0px 4px 8px",
+      },
+
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0) rotate(15deg)" },
+          "50%": { transform: "translateY(-14px) rotate(15deg)" },
+        },
+      },
+      animation: {
+        float: "float 3s ease-in-out infinite",
+      },
+
+      backgroundClip: {
+        text: "text",
+      },
+      textFillColor: {
+        transparent: "transparent",
+      },
+    },
   },
   plugins: [],
+  safelist: ["text-gradient"],
 };
